c009c8cbb8444b887dab433b9495378152716d46
Jassiel Ovando <jassielovando@protonmail.com>
2026-03-06 02:25:36 -0400

    got it

--- src/lib/pcre2.zig
+++ src/lib/pcre2.zig
@@ -1,7 +1,2 @@
 //! PCRE2 bindings.
 const std = @import("std");
-
-pub const pcre2 = "hola";
-pub const str = struct {
-    a: i32,
-};
--- src/lib/root.zig
+++ src/lib/root.zig
@@ -1,4 +1,3 @@
 //! Regular expressions.
-// For some reason, this won't work if I try to use imports in the build.zig file, so I'm just directly importing the zig modules.
 pub const pcre2 = @import("pcre2.zig");
 pub const re2 = @import("re2.zig");
